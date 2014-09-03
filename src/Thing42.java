2573648f77b543a9628fed0657cb9df2e9ad2d38
jgillham <jgillham@MS11WD.winad.msudenver.edu.>
2014-09-03 16:27:12 -0600

    Styled.

--- src/Thing42.java
+++ src/Thing42.java
@@ -1,84 +1,93 @@
+//~--- JDK imports ------------------------------------------------------------
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 
 /**
- * Implementation of all elements of the {@code Thing42orNull} interface. 
- * 
+ * Implementation of all elements of the {@code Thing42orNull} interface.
+ *
  * @author Group 3 - MW - From Jody Paul's API
  * @version CS4250 Fall 2014 - API - (31 August 2014)
  */
-public class Thing42<K,D> implements Thing42orNull<K,D>{
+public class Thing42<K, D> implements Thing42orNull<K, D> {
+    final private K                         key;
+    final private long                      level;
+    private D                               data;
+    private Collection<Thing42orNull<K, D>> peers;
+    private List<Thing42orNull<K, D>>       pool;
 
-    final private K key;
-    final private long level;
-    private D data;
-    private Collection<Thing42orNull<K,D>> peers;
-    private List<Thing42orNull<K,D>> pool;
-    
     /**
      * Constructs an empty Thing42 with it's level set to 0.
      */
-    public Thing42(){
-        this.key = null;
+    public Thing42() {
+        this.key   = null;
         this.level = 0;
-        this.data = null;
+        this.data  = null;
         this.peers = null;
-        this.pool = null;
+        this.pool  = null;
     }
+
     /**
      * Construct a Thing42
      * whose value is specified by the parameter.
-     * @param key a generic attribute 
+     * @param key a generic attribute
      * @param level integer-valued attribute
      * @param data generic mutable attribute
      */
-    public Thing42(final K key, final long level, final D data){
-        this.key = key;
+    public Thing42(final K key, final long level, final D data) {
+        this.key   = key;
         this.level = level;
-        this.data = data;
-        this.peers = new HashSet<Thing42orNull<K,D>>();
-        this.pool = new ArrayList<Thing42orNull<K,D>>();
+        this.data  = data;
+        this.peers = new HashSet<Thing42orNull<K, D>> ();
+        this.pool  = new ArrayList<Thing42orNull<K, D>> ();
     }
-    
+
     /**
      * Add a peer to this object.
-     * 
+     *
      * @param newPeer the peer to be added
-     * @throws NullPointerException if the specified peer is null 
+     * @throws NullPointerException if the specified peer is null
      */
-    public void addPeer(Thing42orNull<K,D> newPeer) {
-        if (newPeer.equals(null)) { throw new NullPointerException(); }
-        HashSet<Thing42orNull<K,D>> peerSet = new HashSet<Thing42orNull<K,D>>();        
-        peerSet = (HashSet<Thing42orNull<K,D>>)this.getPeersAsCollection();
+    public void addPeer(Thing42orNull<K, D> newPeer) {
+        if (newPeer.equals(null)) {
+            throw new NullPointerException();
+        }
+
+        HashSet<Thing42orNull<K, D>> peerSet =
+                new HashSet<Thing42orNull<K, D>> ();
+
+        peerSet = (HashSet<Thing42orNull<K, D>> ) this.getPeersAsCollection();
         peerSet.add(newPeer);
         this.peers = peerSet;
     }
-    
+
     /**
      * Append a member to the pool of this object.
-     * 
+     *
      * @param newMember the object to be appended to the pool
      * @throws NullPointerException if the specified item is null
      */
-    public void appendToPool(Thing42orNull<K,D> newMember) {        
-        if (newMember.equals(null)) { throw new NullPointerException(); }
-        else{ this.pool.add(newMember); } 
+    public void appendToPool(Thing42orNull<K, D> newMember) {
+        if (newMember.equals(null)) {
+            throw new NullPointerException();
+        } else {
+            this.pool.add(newMember);
+        }
     }
-    
+
     /**
      * Access the data of this Thing42.
-     * 
+     *
      * @return the data of this object
      */
     public D getData() {
         return this.data;
     }
-    
+
     /**
      * Access the key of this Thing42.
-     * 
+     *
      * @return the key of this object
      */
     public final K getKey() {
@@ -87,115 +96,153 @@ public class Thing42<K,D> implements Thing42orNull<K,D>{
 
     /**
      * Access the level of this Thing42.
-     * 
+     *
      * @return the level of this object
      */
     public final long getLevel() {
         return this.level;
     }
-    
+
     /**
      * Access a peer matching the specified key.
      * @param key the search key
-     * @return any peer known by this object that matches the given key; 
+     * @return any peer known by this object that matches the given key;
      * null if no match
      */
-    public Thing42orNull<K,D> getOnePeer(K key) {
-        if (this.isEmptyPeer()){return null;}
-        Thing42orNull newThing42 = null;        
-        for(Thing42orNull peer 
-                : (HashSet<Thing42orNull<K,D>>)this.getPeersAsCollection(key)){
-            if(peer.getKey().equals(key)){
+    public Thing42orNull<K, D> getOnePeer(K key) {
+        if (this.isEmptyPeer()) {
+            return null;
+        }
+
+        Thing42orNull newThing42 = null;
+
+        for (Thing42orNull peer : (HashSet<Thing42orNull<K, D>>)
+                this.getPeersAsCollection(key)) {
+            if (peer.getKey().equals(key)) {
                 newThing42 = peer;
+
                 break;
-            }                    
-        }           
+            }
+        }
+
         return newThing42;
     }
 
     /**
      * Access all peers.
-     * @return all peers known by this object; 
+     * @return all peers known by this object;
      * if no peers then returns a collection with size() == 0.
      */
     public Collection getPeersAsCollection() {
-         if (this.isEmptyPeer()){return new HashSet<Thing42orNull<K,D>>();}
-         return this.peers;
+        if (this.isEmptyPeer()) {
+            return new HashSet<Thing42orNull<K, D>>();
+        }
+
+        return this.peers;
     }
 
     /**
      * Access all peers matching the specified key.
      * @param key the search key
-     * @return all peers known by this object that match the given key; 
+     * @return all peers known by this object that match the given key;
      * if no peer matches then returns a collection with size() == 0.
      */
     public Collection getPeersAsCollection(K key) {
-        if (this.isEmptyPeer()){return new HashSet<Thing42orNull<K,D>>();}
-        Collection<Thing42orNull<K,D>> peerSet = new HashSet<Thing42orNull<K,D>>();
-        for (Thing42orNull peer : this.peers){
-            if(peer.getKey().equals(key)){peerSet.add(peer);}
+        if (this.isEmptyPeer()) {
+            return new HashSet<Thing42orNull<K, D>>();
         }
+
+        Collection<Thing42orNull<K, D>> peerSet =
+                new HashSet<Thing42orNull<K, D>>();
+
+        for (Thing42orNull peer : this.peers) {
+            if (peer.getKey().equals(key)) {
+                peerSet.add(peer);
+            }
+        }
+
         return peerSet;
     }
 
     /**
      * Access all members of the pool.
-     * @return all members of the pool known by this object; 
+     * @return all members of the pool known by this object;
      * if no members then returns a list with size() == 0.
      */
     public List getPoolAsList() {
-        if (this.isEmptyPool()){return new ArrayList<Thing42orNull<K,D>>();}
+        if (this.isEmptyPool()) {
+            return new ArrayList<Thing42orNull<K, D>>();
+        }
+
         return this.pool;
     }
 
     /**
      * Remove a single instance of the specified object from this object's pool.
-     * 
+     *
      * @param member the member to be removed from the pool
      * @return true if a pool member was removed as a result of this call
      * @throws NullPointerException if the specified parameter is null
      */
-    public boolean removeFromPool(Thing42orNull<K,D> member) {
-        if (member.equals(null)) { throw new NullPointerException(); }
-        if (this.isEmptyPool()){return false;}
-        List<Thing42orNull<K,D>> poolList = this.getPoolAsList();        
+    public boolean removeFromPool(Thing42orNull<K, D> member) {
+        if (member.equals(null)) {
+            throw new NullPointerException();
+        }
+
+        if (this.isEmptyPool()) {
+            return false;
+        }
+
+        List<Thing42orNull<K, D>> poolList = this.getPoolAsList();
+
         if (poolList.remove(member)) {
             this.pool = poolList;
+
             return true;
         }
+
         return false;
     }
 
     /**
      * Remove a single instance of the specified peer from this object.
-     * 
+     *
      * @param peer the peer to be removed
      * @return true if a peer was removed as a result of this call
      * @throws NullPointerException if the specified peer is null
      */
-    public boolean removePeer(Thing42orNull<K,D> peer){
-        if (peer.equals(null)) { throw new NullPointerException(); }
-        if (this.isEmptyPeer()){return false;}
-        //Collection<Thing42orNull> peerSet = new HashSet<Thing42orNull>();
-        //peerSet = (HashSet<Thing42orNull>)this.getPeersAsCollection();
-        Collection<Thing42orNull<K,D>> peerSet 
-                = (HashSet<Thing42orNull<K,D>>)this.getPeersAsCollection();        
-        if (peerSet.remove(peer)){
+    public boolean removePeer(Thing42orNull<K, D> peer) {
+        if (peer.equals(null)) {
+            throw new NullPointerException();
+        }
+
+        if (this.isEmptyPeer()) {
+            return false;
+        }
+
+        // Collection<Thing42orNull> peerSet = new HashSet<Thing42orNull>();
+        // peerSet = (HashSet<Thing42orNull>)this.getPeersAsCollection();
+        Collection<Thing42orNull<K, D>> peerSet =
+                (HashSet<Thing42orNull<K, D>>) this.getPeersAsCollection();
+
+        if (peerSet.remove(peer)) {
             this.peers = peerSet;
+
             return true;
         }
+
         return false;
     }
 
     /**
      * Modify the data of this Thing42.
-     * 
+     *
      * @param newData the updated data for this object
      */
     public void setData(D newData) {
         this.data = newData;
-    }   
-    
+    }
+
     /**
      * Determine if Collection of peers is empty
      * @return <code>true</code> if the Collection of peers is empty;
@@ -204,7 +251,7 @@ public class Thing42<K,D> implements Thing42orNull<K,D>{
     private boolean isEmptyPeer() {
         return this.peers.isEmpty();
     }
-    
+
     /**
      * Determine if pool List is empty
      * @return <code>true</code> if the pool List is empty;
@@ -212,26 +259,31 @@ public class Thing42<K,D> implements Thing42orNull<K,D>{
      */
     private boolean isEmptyPool() {
         return this.pool.isEmpty();
-    }    
-    
+    }
+
     /**
-     * <p>Compare the equality of two Thing42 objects to ensure they have the same
-     * Key, Data, level, pool collection, and peers collection. Since K and D are generics
-     * equals must also compare K and D of both objects to make sure they are the same type
+     * <p>Compare the equality of two Thing42 objects to ensure they have the
+     * same
+     * Key, Data, level, pool collection, and peers collection. Since K and D
+     * are generics
+     * equals must also compare K and D of both objects to make sure they are
+     * the same type
      *
-     * @param otherThing the second Thing42 object to be compared with this Thing42 object
+     * @param otherThing the second Thing42 object to be compared with this
+     * Thing42 object
      * @return true if the two objects are equal
      */
-    public boolean equals(Thing42orNull<K,D> otherThing){
+    public boolean equals(Thing42orNull<K, D> otherThing) {
         return false;
-    };
-    
+    }
+
     /**
      * Returns the hash code value for this Thing42 object
      *
      * @return hashCode int for this Thing42 object
      */
-    public int hashCode(){
+    @Override
+    public int hashCode() {
         return 0;
-    };
+    }
 }
--- test/Thing42Test.java
+++ test/Thing42Test.java
@@ -1,41 +1,39 @@
-
-import java.util.HashSet;
-import static org.junit.Assert.*;
+//~--- non-JDK imports --------------------------------------------------------
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+//~--- JDK imports ------------------------------------------------------------
+import java.util.HashSet;
 
 /**
  * Preliminary Programming Project
  * Test Class for Thing42 class
- * 
+ *
  * @author Alex Miscall - From Jody Paul's API
  * @version CS4250 Fall 2014 (22 August 2014)
  */
 public class Thing42Test {
-    
-    //private Thing42<null,null> empty;
-    private Thing42<String,String> rgbBlack;
-    private Thing42<String,String> rgbRed;
-    private Thing42<String,String> rgbRandom;
-    
-    private Thing42<Integer,String> intString1;
-    private Thing42<Integer,String> intString2;
-    
-    private Thing42<Integer,String> nullIntString;  
-    
-    
+
+    // private Thing42<null,null> empty;
+    private Thing42<String, String>  rgbBlack;
+    private Thing42<String, String>  rgbRed;
+    private Thing42<String, String>  rgbRandom;
+    private Thing42<Integer, String> intString1;
+    private Thing42<Integer, String> intString2;
+    private Thing42<Integer, String> nullIntString;
+
     @Before
     public void setUp() {
-        //Black is the 1st level
-        rgbBlack = new Thing42<String,String>("#000000", 1, "Black");
-        rgbRed = new Thing42<String,String>("#FF0000", 2, "Red");
-        rgbRandom = new Thing42<String,String>("#FF123A", 3, "Random");
-        
-        intString1 = new Thing42<Integer,String>(1,1,"one");
-        intString2 = new Thing42<Integer,String>(1,2,"two");
-        
-        nullIntString = new Thing42<Integer,String>();        
-    }        
+
+        // Black is the 1st level
+        rgbBlack      = new Thing42<String, String> ("#000000", 1, "Black");
+        rgbRed        = new Thing42<String, String> ("#FF0000", 2, "Red");
+        rgbRandom     = new Thing42<String, String> ("#FF123A", 3, "Random");
+        intString1    = new Thing42<Integer, String> (1, 1, "one");
+        intString2    = new Thing42<Integer, String> (1, 2, "two");
+        nullIntString = new Thing42<Integer, String> ();
+    }
 
     /**
      * Test of addPeer method, of class Thing42.
@@ -45,23 +43,26 @@ public class Thing42Test {
         assertEquals(0, rgbBlack.getPeersAsCollection().size());
         rgbBlack.addPeer(rgbRed);
         assertEquals(1, rgbBlack.getPeersAsCollection().size());
-        //Should we be able to do this?
+
+        // Should we be able to do this?
         rgbBlack.addPeer(rgbRandom);
         assertEquals(2, rgbBlack.getPeersAsCollection().size());
         assertEquals(true, rgbBlack.getPeersAsCollection().contains(rgbRed));
-        assertEquals(true, rgbBlack.getPeersAsCollection().contains(intString1));
+        assertEquals(true, rgbBlack.getPeersAsCollection()
+                .contains(intString1));
         rgbBlack.addPeer(null);
         assertEquals(3, rgbBlack.getPeersAsCollection().size());
-    }    
-    
+    }
+
     /**
      * Test of addPeer method, of class Thing42.
      * specified peer is null so expected=NullPointerException
      */
-    @Test(expected=NullPointerException.class)
+    @Test(expected = NullPointerException.class)
     public void testAddPeerNPExc() {
         rgbBlack.addPeer(null);
     }
+
     /**
      * Test of appendToPool method, of class Thing42.
      */
@@ -71,23 +72,27 @@ public class Thing42Test {
         rgbBlack.appendToPool(rgbRed);
         rgbBlack.appendToPool(rgbRandom);
         assertEquals(2, rgbBlack.getPoolAsList().size());
-        Thing42orNull<String,String> thing1 
-                = (Thing42orNull)rgbBlack.getPoolAsList().get(0);
+
+        Thing42orNull<String, String> thing1 =
+                (Thing42orNull) rgbBlack.getPoolAsList().get(0);
+
         assertEquals("Red", thing1.getData());
-        Thing42orNull<String,String> thing2 
-                = (Thing42orNull)rgbBlack.getPoolAsList().get(1);
+
+        Thing42orNull<String, String> thing2 =
+                (Thing42orNull) rgbBlack.getPoolAsList().get(1);
+
         assertEquals("one", thing2.getData());
     }
-    
+
     /**
      * Test of appendToPool method, of class Thing42.
      * specified item is null so expected=NullPointerException
      */
-    @Test(expected=NullPointerException.class)
-    public void testAppendToPoolNPExc(){
+    @Test(expected = NullPointerException.class)
+    public void testAppendToPoolNPExc() {
         rgbBlack.appendToPool(null);
     }
-    
+
     /**
      * Test of getData method, of class Thing42.
      */
@@ -95,7 +100,7 @@ public class Thing42Test {
     public void testGetData() {
         assertEquals("Black", rgbBlack.getData());
         assertEquals("one", intString1.getData());
-        assertEquals(null, nullIntString.getData());        
+        assertEquals(null, nullIntString.getData());
     }
 
     /**
@@ -104,8 +109,8 @@ public class Thing42Test {
     @Test
     public void testGetKey() {
         assertEquals("#000000", rgbBlack.getKey());
-        assertEquals(1, (long)intString1.getKey());
-        assertEquals(null, nullIntString.getKey()); 
+        assertEquals(1, (long) intString1.getKey());
+        assertEquals(null, nullIntString.getKey());
     }
 
     /**
@@ -126,30 +131,36 @@ public class Thing42Test {
         assertEquals(0, rgbBlack.getPeersAsCollection().size());
         rgbBlack.addPeer(rgbRed);
         assertEquals(1, rgbBlack.getPeersAsCollection().size());
-        Thing42orNull<String,String> onePeer = rgbBlack.getOnePeer("#FF0000"); 
+
+        Thing42orNull<String, String> onePeer =
+                rgbBlack.getOnePeer("#FF0000");
+
         assertEquals("Red", onePeer.getData());
     }
-    
+
     /**
      * Test of getOnePeer method, of class Thing42.
      * specified Key is null so NullPointerException is thrown
      */
-    @Test(expected=NullPointerException.class)    
-    public void testGetOnePeerNPExc(){
+    @Test(expected = NullPointerException.class)
+    public void testGetOnePeerNPExc() {
         assertEquals(0, rgbBlack.getPeersAsCollection().size());
         rgbBlack.addPeer(rgbRed);
-        assertEquals(1, rgbBlack.getPeersAsCollection().size());        
-        Thing42orNull<String,String> anotherPeer = rgbBlack.getOnePeer("#FF000");
+        assertEquals(1, rgbBlack.getPeersAsCollection().size());
+
+        Thing42orNull<String, String> anotherPeer =
+                rgbBlack.getOnePeer("#FF000");
+
         assertEquals(null, anotherPeer.getData());
     }
-    
+
     /**
      * Test of getPeersAsCollection method, of class Thing42.
      */
     @Test
-    public void testGetPeersAsCollection_0args() {   
-        rgbBlack = new Thing42<String,String>("#000000", 1, "Black");
-        rgbRed = new Thing42<String,String>("#FF0000", 2, "Red");
+    public void testGetPeersAsCollection_0args() {
+        rgbBlack = new Thing42<String, String>("#000000", 1, "Black");
+        rgbRed   = new Thing42<String, String>("#FF0000", 2, "Red");
         assertEquals(0, rgbBlack.getPeersAsCollection().size());
         rgbBlack.addPeer(rgbRed);
         assertEquals(1, rgbBlack.getPeersAsCollection().size());
@@ -159,14 +170,15 @@ public class Thing42Test {
      * Test of getPeersAsCollection method, of class Thing42.
      */
     @Test
-    public void testGetPeersAsCollection_GenericType() {      
+    public void testGetPeersAsCollection_GenericType() {
         assertEquals(0, rgbBlack.getPeersAsCollection("#FF0000").size());
         rgbBlack.addPeer(rgbRed);
         assertEquals(1, rgbBlack.getPeersAsCollection("#FF0000").size());
-        for(Thing42orNull peer 
-                : (HashSet<Thing42orNull>)rgbBlack.getPeersAsCollection("#FF0000")){
+
+        for (Thing42orNull peer : (HashSet<Thing42orNull>)
+                rgbBlack.getPeersAsCollection("#FF0000")) {
             assertTrue(peer.getData().equals("Red"));
-        }        
+        }
     }
 
     /**
@@ -174,16 +186,18 @@ public class Thing42Test {
      */
     @Test
     public void testGetPoolAsList() {
-        //List<Thing42orNull> poolList = rgbBlack.getPoolAsList();
+
+        // List<Thing42orNull> poolList = rgbBlack.getPoolAsList();
         assertEquals(0, rgbBlack.getPoolAsList().size());
         rgbBlack.appendToPool(rgbRed);
         assertEquals(1, rgbBlack.getPoolAsList().size());
-        //add a Thing42 with a different key and data data types
+
+        // add a Thing42 with a different key and data data types
         rgbBlack.appendToPool(rgbRandom);
         assertEquals(2, rgbBlack.getPoolAsList().size());
         rgbBlack.removeFromPool(rgbRed);
         assertEquals(1, rgbBlack.getPoolAsList().size());
-        assertEquals(true, rgbBlack.getPoolAsList().contains(intString1));        
+        assertEquals(true, rgbBlack.getPoolAsList().contains(intString1));
     }
 
     /**
@@ -195,17 +209,23 @@ public class Thing42Test {
         rgbBlack.appendToPool(rgbRed);
         rgbBlack.appendToPool(rgbRandom);
         assertEquals(2, rgbBlack.getPoolAsList().size());
-        Thing42orNull<String,String> thing1 
-                = (Thing42orNull)rgbBlack.getPoolAsList().get(0);
+
+        Thing42orNull<String, String> thing1 =
+                (Thing42orNull) rgbBlack.getPoolAsList().get(0);
+
         assertEquals("Red", thing1.getData());
-        Thing42orNull<String,String> thing2 
-                = (Thing42orNull)rgbBlack.getPoolAsList().get(1);
+
+        Thing42orNull<String, String> thing2 =
+                (Thing42orNull) rgbBlack.getPoolAsList().get(1);
+
         assertEquals("one", thing2.getData());
         assertEquals(true, rgbBlack.removeFromPool(rgbRed));
         assertEquals(false, rgbBlack.removeFromPool(rgbRed));
         assertEquals(1, rgbBlack.getPoolAsList().size());
-        Thing42orNull<String,String> thing3 
-                = (Thing42orNull)rgbBlack.getPoolAsList().get(0);
+
+        Thing42orNull<String, String> thing3 =
+                (Thing42orNull) rgbBlack.getPoolAsList().get(0);
+
         assertEquals("one", thing3.getData());
     }
 
@@ -213,15 +233,15 @@ public class Thing42Test {
      * Test of removeFromPool method, of class Thing42.
      * Specified parameter is null so expected=NullPointerException
      */
-    @Test(expected=NullPointerException.class)    
-    public void testRemoveFromPoolNPExc(){
+    @Test(expected = NullPointerException.class)
+    public void testRemoveFromPoolNPExc() {
         assertEquals(0, rgbBlack.getPoolAsList().size());
         rgbBlack.appendToPool(rgbRed);
         rgbBlack.appendToPool(rgbRandom);
         assertEquals(true, rgbBlack.removeFromPool(rgbRed));
         rgbBlack.removeFromPool(null);
     }
-    
+
     /**
      * Test of removePeer method, of class Thing42.
      */
@@ -230,11 +250,13 @@ public class Thing42Test {
         assertEquals(0, rgbBlack.getPeersAsCollection().size());
         rgbBlack.addPeer(rgbRed);
         assertEquals(1, rgbBlack.getPeersAsCollection().size());
-        //Should we be able to do this?
+
+        // Should we be able to do this?
         rgbBlack.addPeer(rgbRandom);
         assertEquals(2, rgbBlack.getPeersAsCollection().size());
         assertEquals(true, rgbBlack.getPeersAsCollection().contains(rgbRed));
-        assertEquals(true, rgbBlack.getPeersAsCollection().contains(intString1));
+        assertEquals(true, rgbBlack.getPeersAsCollection()
+                .contains(intString1));
         assertEquals(true, rgbBlack.removePeer(rgbRed));
         assertEquals(false, rgbBlack.removePeer(rgbRed));
         assertEquals(false, rgbBlack.getPeersAsCollection().contains(rgbRed));
@@ -245,18 +267,19 @@ public class Thing42Test {
      * Test of removePeer method, of class Thing42.
      * Specified parameter is null so expected=NullPointerException
      */
-    @Test(expected=NullPointerException.class)
-    public void testRemovePeerNPExc(){
+    @Test(expected = NullPointerException.class)
+    public void testRemovePeerNPExc() {
         assertEquals(0, rgbBlack.getPeersAsCollection().size());
         rgbBlack.addPeer(rgbRed);
         assertEquals(1, rgbBlack.getPeersAsCollection().size());
-        //Should we be able to do this?
+
+        // Should we be able to do this?
         rgbBlack.addPeer(rgbRandom);
         assertEquals(2, rgbBlack.getPeersAsCollection().size());
         assertEquals(true, rgbBlack.removePeer(rgbRed));
         assertEquals(false, rgbBlack.removePeer(null));
     }
-    
+
     /**
      * Test of setData method, of class Thing42.
      */
@@ -268,8 +291,8 @@ public class Thing42Test {
         assertEquals("one", intString1.getData());
         intString1.setData("three");
         assertEquals("three", intString1.getData());
-        assertEquals(null, nullIntString.getData()); 
+        assertEquals(null, nullIntString.getData());
         nullIntString.setData("new");
         assertEquals("new", nullIntString.getData());
-    }    
+    }
 }
