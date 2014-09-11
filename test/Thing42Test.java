c3931aebcd192f1c93f7526bb3c3eea64bd70da9
Handro <Handro@10.0.0.6>
2014-09-10 23:56:44 -0600

    Updated Thing42Test class Check Style Errors 3

--- test/Thing42Test.java
+++ test/Thing42Test.java
@@ -1,8 +1,12 @@
 //~--- non-JDK imports --------------------------------------------------------
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  * Preliminary Programming Project Test Class for Thing42 class.
